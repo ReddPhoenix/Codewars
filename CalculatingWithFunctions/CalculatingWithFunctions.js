3398d2d2773c61d7ffab8551e97381a11445bad3
Walter Guerrero <wsguerrero@gmail.com>
2021-08-20 09:24:00 -0600

    solved

--- CalculatingWithFunctions/CalculatingWithFunctions.js
+++ CalculatingWithFunctions/CalculatingWithFunctions.js
@@ -1,17 +1,51 @@
 // Calculation consists of one operation and two numbers
-function calculation (numbers, operation)
+function calculation (numbers, operation) {
+  if(!operation) {
+    return numbers;
+  }
+  return operation(numbers);
+}
 
 // Function for each number
-function zero(operation) {}
-function one(operation) {}
-function two(operation) {}
-function three(operation) {}
-function four(operation) {}
-function five(operation) {}
-function six(operation) {}
-function seven(operation) {}
-function eight(operation) {}
-function nine(operation) {}
+function zero(operation) {
+  return calculation(0, operation);
+}
+
+function one(operation) {
+  return calculation(1, operation);
+}
+
+function two(operation) {
+  return calculation(2, operation);
+}
+
+function three(operation) {
+  return calculation(3, operation);
+}
+
+function four(operation) {
+  return calculation(4, operation);
+}
+
+function five(operation) {
+  return calculation(5, operation);
+}
+
+function six(operation) {
+  return calculation(6, operation);
+}
+
+function seven(operation) {
+  return calculation(7, operation);
+}
+
+function eight(operation) {
+  return calculation(8, operation);
+}
+
+function nine(operation) {
+  return calculation(9, operation);
+}
 
 // Function for each operation
 function plus(firstNumber) {
@@ -20,21 +54,42 @@ function plus(firstNumber) {
     }
   }
   
-  function minus(firstNumber) {
+function minus(firstNumber) {
     return function(secondNumber) {
       return secondNumber - firstNumber;
     }
   }
                                                                                                                                                                                                        
-  function times(firstNumber) {
+function times(firstNumber) {
     return function(secondNumber) {
       return secondNumber * firstNumber;   
     }
   }
   
-  // Division should be integer division '/' and NOT '%'
-  function dividedBy(firstNumber) {
+// Division should be integer division '/' and NOT '%'
+function dividedBy(firstNumber) {
     return function(secondNumber) {
       return secondNumber / firstNumber;
     }
-  }
+}
+  
+// Tests
+describe("Tests", () => {
+  it("test", () => {
+Test.assertEquals(seven(times(five())), 35);
+Test.assertEquals(four(plus(nine())), 13);
+Test.assertEquals(eight(minus(three())), 5);
+Test.assertEquals(six(dividedBy(two())), 3);
+  });
+});
+
+// Test Results:
+// Tests
+// test
+// Test Passed: Value == 35
+// Test Passed: Value == 13
+// Test Passed: Value == 5
+// Test Passed: Value == 3
+// Completed in 2ms
+// Completed in 5ms
+// You have passed all of the tests! :)
