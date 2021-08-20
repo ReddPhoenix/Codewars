1c86dd1ad7d1ebcca8e7dd87ef9c0780fea1c82a
Walter Guerrero <wsguerrero@gmail.com>
2021-08-19 23:59:49 -0600

    created functions

--- CalculatingWithFunctions/CalculatingWithFunctions.js
+++ CalculatingWithFunctions/CalculatingWithFunctions.js
@@ -1,14 +1,19 @@
-function zero() {}
-function one() {}
-function two() {}
-function three() {}
-function four() {}
-function five() {}
-function six() {}
-function seven() {}
-function eight() {}
-function nine() {}
+// Calculation consists of one operation and two numbers
+function calculation (numbers, operation)
 
+// Function for each number
+function zero(operation) {}
+function one(operation) {}
+function two(operation) {}
+function three(operation) {}
+function four(operation) {}
+function five(operation) {}
+function six(operation) {}
+function seven(operation) {}
+function eight(operation) {}
+function nine(operation) {}
+
+// Function for each operation
 function plus() {}
 function minus() {}
 function times() {}
