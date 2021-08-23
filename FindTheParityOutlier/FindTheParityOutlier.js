88ac47a7db66aa1a2686de1217af1266b8a67ba1
Walter Guerrero <wsguerrero@gmail.com>
2021-08-22 23:59:01 -0600

    pseudocode

---
+++ FindTheParityOutlier/FindTheParityOutlier.js
@@ -0,0 +1,8 @@
+// Can create two arrays.
+// One array for odd numbers.
+
+
+
+// One array for even numbers.
+// Value can be divided using % and if the remainder is not 0 then pushed to odd array.
+// Value is pushed to even if remainder is 0.
