b4b0e64e2ec97570787553b7f6eda0497f4c352d
Walter Guerrero <wsguerrero@gmail.com>
2021-09-13 23:47:18 -0600

    completed

--- SimplePigLatin/SimplePigLatin.js
+++ SimplePigLatin/SimplePigLatin.js
@@ -1,8 +1,10 @@
-function pigIt(str){
+function pigIt(str) {
   const array = str.split(' ');
   
-  return array
-  // RegEx
-  pigLatin.match(/[A-z]/i)
+  return array.map((pigLatin) => {
     
-  }
+    // RegEx
+    return pigLatin.match(/[A-z]/i) ? `${pigLatin.substr(1)}${pigLatin.substr(0, 1)}ay` : pigLatin;
+  
+  }).join(' ')
+}
